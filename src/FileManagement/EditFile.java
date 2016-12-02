a8b8b44bb285fef8b08d74bb6c8d465428236b38
alina52 <alinawang1125@gmail.com>
2016-12-02 11:41:25 +0800

    no message

--- src/FileManagement/EditFile.java
+++ src/FileManagement/EditFile.java
@@ -1,4 +1,52 @@
 package FileManagement;
 
 public class EditFile {
+	public EditFile(File file, int id) {
+        super("文件编辑");
+        setDefaultSettings();
+
+        this.id = id;
+        loadItems();
+
+        this.addWindowListener(new windowsAdapter());
+        this.setVisible(true);
+
+
+        Map<ArrayList<Integer>,String> get = DataManager.loadFile(file.getBlock());
+        pointers = (ArrayList<Integer>) get.keySet().toArray()[0];
+        String text = get.get(pointers);
+        setContext(text);
+    }
+
+    private void loadItems() {
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+
+        JScrollPane jScrollPane = new JScrollPane();
+        jScrollPane.setViewportView(textArea);
+        this.add(jScrollPane);
+    }
+
+    private void setDefaultSettings() {
+        setBounds(200, 200, 610, 450);
+        setResizable(false);
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+        textArea.setText(context);
+    }
+
+    private class windowsAdapter extends WindowAdapter {
+        @Override
+        public void windowClosing(WindowEvent e) {
+            DataManager.modifyFile(id,pointers,textArea.getText());
+            FileTree.setCurrent(FileTree.getCurrent());
+            super.windowClosing(e);
+        }
+    }
 }
